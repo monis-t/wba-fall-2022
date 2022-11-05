1a422edb6339773ae2598c091f4934e809e21546
Monis <62466131+monis-t@users.noreply.github.com>
2022-11-05 13:53:20 +0300

    adding hello

---
+++ hello/hello.rs
@@ -0,0 +1,18 @@
+// priting a string using rustc.
+// steps :
+// declare a function with the desired output.
+// mavigate to the file location on the terminal.
+// type in "rustc filename.rs"
+// the command creates three files.
+//  -executable.
+//  -db.
+//  -main.
+// This is however an undesireable method for large 
+// codebases. 
+// In the following files, we will utilize a powerful 
+// tool that takes care of everything for us.
+
+
+fn main() {
+    println!("Hello World !")
+}
